fc57a9e6c92f489e6aa481d8d595ceab1feecb6f
adsdeworst <advaithvecham@hotmail.com>
2026-01-03 19:51:09 -0800

    Initial Commit

---
+++ db/main.py
@@ -0,0 +1,19 @@
+import psycopg2
+from sqlalchemy import create_engine, text
+
+DB_URL = "postgresql+psycopg2://advaithvecham:advaith@localhost:5432/mydb"
+engine = create_engine(DB_URL, echo=True)
+with engine.connect() as conn:
+    result = conn.execute(text(
+        """
+        INSERT INTO person (id, name, age, gender)
+        VALUES (1, 'Advaith', 18, 'm');
+        """
+        )
+    )
+    conn.commit()
+
+from sqlalchemy.orm import Session
+
+session = Session(engine)
+session.execute
